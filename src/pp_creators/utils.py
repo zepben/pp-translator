e766fcf040b067828778b00ae8b0986c2c208d3c
Roberto Marquez <roberto.marquez@zepben.com>
2021-08-13 16:25:15 +1000

    - Updated get_upstream_end_to_tns method to account for different phases.
    Signed-off-by: Roberto Marquez <roberto.marquez@zepben.com>

--- src/pp_creators/utils.py
+++ src/pp_creators/utils.py
@@ -13,6 +13,8 @@ def get_upstream_end_to_tns(
     return [(end, tn) for (end, tn) in ends_to_topological_nodes
             if tn is not None
             and end is not None
-            # TODO: How to account for the fact that this can have phases other than A and they can have
-            #  different directions??. This hack works only for a balanced system
-            and end.terminal.traced_phases.direction_normal(SinglePhaseKind.A).has(PhaseDirection.IN)]
+            # TODO: How to account for the fact you can have phases with different directions??
+            and (end.terminal.traced_phases.direction_normal(SinglePhaseKind.A).has(PhaseDirection.IN)
+                 or end.terminal.traced_phases.direction_normal(SinglePhaseKind.B).has(PhaseDirection.IN)
+                 or end.terminal.traced_phases.direction_normal(SinglePhaseKind.C).has(PhaseDirection.IN))
+            ]
