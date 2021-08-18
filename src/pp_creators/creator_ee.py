5811b972b8821e4ee692d82be1d67a66e3e8e12b
Roberto Marquez <roberto.marquez@zepben.com>
2021-08-18 13:03:18 +1000

    - Added functionality to the PandaPowerNetworkCreatorEE to generate pandapower models with asymmetric loads.
    Signed-off-by: Roberto Marquez <roberto.marquez@zepben.com>

--- src/pp_creators/creator_ee.py
+++ src/pp_creators/creator_ee.py
@@ -4,17 +4,30 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import logging
-from typing import FrozenSet, Tuple, Iterable, List, Optional, Callable, Dict
+from typing import FrozenSet, Tuple, List, Optional, Callable, Dict, Iterable
 
 import pandapower as pp
 from zepben.evolve import Terminal, NetworkService, AcLineSegment, PowerTransformer, EnergyConsumer, \
     PowerTransformerEnd, ConductingEquipment, \
-    PowerElectronicsConnection, Location, BusBranchNetworkCreator, EnergySource, Switch, Junction, BusbarSection
+    PowerElectronicsConnection, Location, BusBranchNetworkCreator, EnergySource, Switch, Junction, BusbarSection, \
+    SinglePhaseKind, PhaseCode, TracedPhases
 
 from pp_creators.utils import get_upstream_end_to_tns
 from pp_creators.validators.validator import PandaPowerNetworkValidator
 
 __all__ = ["PandaPowerNetworkCreatorEE", "PpElement"]
+
+P_OPTIONS = {
+    SinglePhaseKind.A: "p_a_mw",
+    SinglePhaseKind.B: "p_b_mw",
+    SinglePhaseKind.C: "p_c_mw"
+}
+
+Q_OPTIONS = {
+    SinglePhaseKind.A: "q_a_mvar",
+    SinglePhaseKind.B: "q_b_mvar",
+    SinglePhaseKind.C: "q_c_mvar"
+}
 
 
 class PpElement:
@@ -31,12 +44,14 @@ class PandaPowerNetworkCreatorEE(
     def __init__(
             self, *,
             logger: logging.Logger,
+            asymmetric_load: bool = False,
             vm_pu: float = 1.0,
             load_provider: Callable[[ConductingEquipment], Tuple[float, float]] = lambda x: (0, 0),
             pec_load_provider: Callable[[ConductingEquipment], Tuple[float, float]] = lambda x: (0, 0)
     ):
         self.vm_pu = vm_pu
         self.logger = logger
+        self.asymmetric_load = asymmetric_load
         self.load_provider = load_provider
         self.pec_load_provider = pec_load_provider
 
@@ -158,6 +173,8 @@ class PandaPowerNetworkCreatorEE(
     ) -> PpElement:
         # Create Bus
         coord: Tuple[float, float] = [(p.x_position, p.y_position) for p in power_transformer.location.points][0]
+        traced_phases = next(power_transformer.terminals).traced_phases
+        p_keys, q_keys = _get_active_phase_keys(traced_phases)
 
         bus_idx = pp.create_bus(
             bus_branch_network,
@@ -169,23 +186,43 @@ class PandaPowerNetworkCreatorEE(
 
         # Create Load
         p, q = self.load_provider(power_transformer)
-        load_idx = pp.create_load(
-            bus_branch_network,
-            bus=bus_idx,
-            p_mw=p / 1000000,
-            q_mvar=q / 1000000,
-            name=f"{power_transformer.name}_load",
-        )
+
+        if self.asymmetric_load:
+            load_idx = pp.create_asymmetric_load(
+                bus_branch_network,
+                bus=bus_idx,
+                name=f"{power_transformer.name}_load",
+                **{k: p / 1000000 for k in p_keys},
+                **{k: q / 1000000 for k in q_keys}
+            )
+        else:
+            load_idx = pp.create_load(
+                bus_branch_network,
+                bus=bus_idx,
+                p_mw=p / 1000000,
+                q_mvar=q / 1000000,
+                name=f"{power_transformer.name}_load",
+            )
 
         # Create PV
         p, q = self.pec_load_provider(power_transformer)
-        pv_load_idx = pp.create_sgen(
-            bus_branch_network,
-            bus=bus_idx,
-            p_mw=p / 1000000,
-            q_mvar=q / 1000000,
-            name=f"{power_transformer.name}_sgen",
-        )
+
+        if self.asymmetric_load:
+            pv_load_idx = pp.create_asymmetric_sgen(
+                bus_branch_network,
+                bus=bus_idx,
+                name=f"{power_transformer.name}_load",
+                **{k: p / 1000000 for k in p_keys},
+                **{k: q / 1000000 for k in q_keys}
+            )
+        else:
+            pv_load_idx = pp.create_sgen(
+                bus_branch_network,
+                bus=bus_idx,
+                p_mw=p / 1000000,
+                q_mvar=q / 1000000,
+                name=f"{power_transformer.name}_sgen",
+            )
 
         mapped_elements[f"bus:{bus_idx}"] = bus_element
         mapped_elements[f"load:{load_idx}"] = PpElement(load_idx, "load")
@@ -215,6 +252,28 @@ class PandaPowerNetworkCreatorEE(
             connected_topological_node: PpElement,
             node_breaker_network: NetworkService
     ) -> Dict[str, PpElement]:
+        if self.asymmetric_load:
+            return self._create_asymmetric_load(
+                bus_branch_network,
+                energy_consumer,
+                connected_topological_node,
+                node_breaker_network
+            )
+        else:
+            return self._create_load(
+                bus_branch_network,
+                energy_consumer,
+                connected_topological_node,
+                node_breaker_network
+            )
+
+    def _create_load(
+            self,
+            bus_branch_network: pp.pandapowerNet,
+            energy_consumer: EnergyConsumer,
+            connected_topological_node: PpElement,
+            node_breaker_network: NetworkService
+    ) -> Dict[str, PpElement]:
         p, q = self.load_provider(energy_consumer)
         load_idx = pp.create_load(
             bus_branch_network,
@@ -226,12 +285,53 @@ class PandaPowerNetworkCreatorEE(
 
         return {f"load:{load_idx}": PpElement(load_idx, "load")}
 
+    def _create_asymmetric_load(
+            self,
+            bus_branch_network: pp.pandapowerNet,
+            energy_consumer: EnergyConsumer,
+            connected_topological_node: PpElement,
+            node_breaker_network: NetworkService
+    ) -> Dict[str, PpElement]:
+        p, q = self.load_provider(energy_consumer)
+        p_keys, q_keys = _get_active_phase_keys(next(energy_consumer.terminals).traced_phases)
+        load_idx = pp.create_asymmetric_load(
+            bus_branch_network,
+            bus=connected_topological_node.index,
+            name=energy_consumer.name,
+            **{k: p / 1000000 for k in p_keys},
+            **{k: q / 1000000 for k in q_keys}
+        )
+
+        return {f"load:{load_idx}": PpElement(load_idx, "load")}
+
     def power_electronics_connection_creator(
             self,
             bus_branch_network: pp.pandapowerNet,
             power_electronics_connection: PowerElectronicsConnection,
             connected_topological_node: PpElement,
             node_breaker_network: NetworkService,
+    ) -> Dict[str, PpElement]:
+        if self.asymmetric_load:
+            return self._create_asymmetric_sgen(
+                bus_branch_network,
+                power_electronics_connection,
+                connected_topological_node,
+                node_breaker_network
+            )
+        else:
+            return self._create_sgen(
+                bus_branch_network,
+                power_electronics_connection,
+                connected_topological_node,
+                node_breaker_network
+            )
+
+    def _create_sgen(
+            self,
+            bus_branch_network: pp.pandapowerNet,
+            power_electronics_connection: PowerElectronicsConnection,
+            connected_topological_node: PpElement,
+            node_breaker_network: NetworkService
     ) -> Dict[str, PpElement]:
         p, q = self.pec_load_provider(power_electronics_connection)
         load_idx = pp.create_sgen(
@@ -240,6 +340,25 @@ class PandaPowerNetworkCreatorEE(
             p_mw=p / 1000000,
             q_mvar=q / 1000000,
             name=power_electronics_connection.name
+        )
+
+        return {f"sgen:{load_idx}": PpElement(load_idx, "sgen")}
+
+    def _create_asymmetric_sgen(
+            self,
+            bus_branch_network: pp.pandapowerNet,
+            power_electronics_connection: PowerElectronicsConnection,
+            connected_topological_node: PpElement,
+            node_breaker_network: NetworkService
+    ) -> Dict[str, PpElement]:
+        p, q = self.pec_load_provider(power_electronics_connection)
+        p_keys, q_keys = _get_active_phase_keys(next(power_electronics_connection.terminals).traced_phases)
+        load_idx = pp.create_asymmetric_sgen(
+            bus_branch_network,
+            bus=connected_topological_node.index,
+            name=power_electronics_connection.name,
+            **{k: p / 1000000 for k in p_keys},
+            **{k: q / 1000000 for k in q_keys}
         )
 
         return {f"sgen:{load_idx}": PpElement(load_idx, "sgen")}
@@ -261,6 +380,22 @@ class PandaPowerNetworkCreatorEE(
 
     def validator_creator(self) -> PandaPowerNetworkValidator:
         return PandaPowerNetworkValidator(logger=self.logger)
+
+
+def _get_active_phase_keys(traced_phases: TracedPhases) -> Tuple[List[str], List[str]]:
+    p_keys = []
+    q_keys = []
+
+    for nominal_phase in PhaseCode.ABC.single_phases:
+        if traced_phases.phase_normal(nominal_phase) != SinglePhaseKind.NONE:
+            p_key = P_OPTIONS.get(nominal_phase)
+            q_key = Q_OPTIONS.get(nominal_phase)
+            if p_key:
+                p_keys.append(p_key)
+            if q_key:
+                q_keys.append(q_key)
+
+    return p_keys, q_keys
 
 
 def _create_id_from_terminals(ts: Iterable[Terminal]):
