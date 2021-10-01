b67fc78fb7f4eac4d3820b95d6607b5c40d399c5
Kurt Greaves <kurt.greaves@zepben.com>
2021-10-01 16:10:33 +1000

    Prepare for release
    Signed-off-by: Kurt Greaves <kurt.greaves@zepben.com>

--- setup.py
+++ setup.py
@@ -1,20 +1,9 @@
-"""
-Copyright 2019 Zeppelin Bend Pty Ltd
-This file is part of ejsonbend.
+#  Copyright 2021 Zeppelin Bend Pty Ltd
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-ejsonbend is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-ejsonbend is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with ejsonbend.  If not, see <https://www.gnu.org/licenses/>.
-"""
 from glob import glob
 from os.path import basename
 from os.path import splitext
@@ -24,9 +13,24 @@ from setuptools import setup, find_packages
 test_deps = ["pytest", "pytest-cov", "pytest-asyncio", "hypothesis<6"]
 setup(
     name="pp-translator",
+    description="Library for translating Zepben CIM network models to pandapower models",
     version="0.1.0",
+    url="https://github.com/zepben/pp-translator",
+    author="Zeppelin Bend",
+    author_email="oss@zepben.com",
+    license="MPL 2.0",
+    classifiers=[
+         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
+         "Programming Language :: Python :: 3.9",
+         "Programming Language :: Python :: 3.10",
+         "Operating System :: OS Independent"
+     ],
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    python_requires='>=3.7',
     py_modules=[splitext(basename(path))[0] for path in glob('src/**/*.py')],
     install_requires=[
         "zepben.evolve==0.25.0b1",
